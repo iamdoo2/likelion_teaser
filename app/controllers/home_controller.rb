6c7643ae0f67d0ff54479d36369cc8f216185a8e
Duhee Lee <doo2@wafflestudio.com>
2013-01-21 03:34:37 +0900

    rc.1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     @new_mail = SnuMail.new
+    @count = SnuMail.count
+  end
+
+  def submit_email
+    new_mail = SnuMail.new
+    new_mail.address = params[:address]
+    new_mail.save
+    render :text => "OK"
   end
 end
